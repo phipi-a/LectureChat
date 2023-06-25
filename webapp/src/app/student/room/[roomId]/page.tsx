59780c47b612729f0b0fc5cde701434214b09a70
philipp <phpi@gmx.de>
2023-06-25 02:34:02 +0200

    add feature: audio transcription

--- webapp/src/app/student/room/[roomId]/page.tsx
+++ webapp/src/app/student/room/[roomId]/page.tsx
@@ -1,30 +1,46 @@
 "use client";
 import Image from "next/image";
 import styles from "./page.module.css";
-import useLocalStorage from "use-local-storage";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { Typography } from "@mui/material";
 import { useGetData } from "@/lib/utils/supabase/supabaseData";
 import { supabase } from "@/common/modules/supabase/supabaseClient";
 import { CenteredLoading } from "@/common/components/CenteredLoading";
+import { Database } from "@/common/constants/supabaseTypes";
+import {
+  PostgrestResponse,
+  PostgrestSingleResponse,
+} from "@supabase/supabase-js";
+
+// shortcut Database["public"]["Tables"]["data"]["Row"]
 
 export default function Room({ params }: { params: { roomId: string } }) {
-  const [data, setData] = useState<any>("");
+  const [data, setData] = useState<
+    Database["public"]["Tables"]["data"]["Row"][]
+  >([]);
+  const dataRef = useRef(data);
+
   const { roomId } = params;
+  const room = useGetData(
+    [roomId, "room"],
+    supabase.from("room_access").select("*").eq("room_id", roomId).single()
+  );
   const initData = useGetData(
     [roomId, "initData"],
-    supabase!.from("data").select("*").eq("room_id", roomId).single(),
+    supabase!.from("data").select("*").eq("room_id", roomId),
     {
-      onSuccess(data) {
-        console.log(data);
+      onSuccess(
+        data: PostgrestSingleResponse<
+          Database["public"]["Tables"]["data"]["Row"][]
+        >
+      ) {
         if (data.error) {
           console.log("erro", data);
         } else {
-          setData(data?.data?.data);
+          console.log("data", data);
+          setData(data.data);
+          dataRef.current = data.data;
         }
-      },
-      onError(error) {
-        console.log(error);
       },
     }
   );
@@ -41,21 +57,46 @@ export default function Room({ params }: { params: { roomId: string } }) {
         },
 
         (payload: any) => {
-          setData(payload.new.data);
-          console.log(payload.new.data);
+          if (payload.eventType === "DELETE") {
+            console.log("Delete received!", payload);
+            dataRef.current = dataRef.current.filter(
+              (d) => d.id !== payload.old.id
+            );
+            setData([...dataRef.current]);
+            return;
+          }
+          if (payload.eventType === "UPDATE") {
+            console.log("Update received!", payload);
+            dataRef.current = dataRef.current.map((d) => {
+              if (d.id === payload.new.id) {
+                return payload.new;
+              }
+              return d;
+            });
+            setData([...dataRef.current]);
+            return;
+          }
+          if (payload.eventType === "INSERT") {
+            console.log("Change received!", payload);
+            dataRef.current.push(payload.new);
+            setData([...dataRef.current]);
+            return;
+          }
         }
       )
       .subscribe();
   }, [supabase, roomId]);
-  if (initData.isLoading) {
+  if (initData.isLoading || room.isLoading) {
     return <CenteredLoading />;
   }
   return (
     <div>
-      <h1>Test</h1>
-      <p>Room ID: {params.roomId}</p>
+      <h1>{room.data?.data?.room_title}</h1>
+      <p>Room ID: {room.data?.data?.room_id}</p>
       <Typography variant="h4">Data</Typography>
-      <Typography variant="body1">{data}</Typography>
+      <Typography variant="body1">
+        {data.map((d) => d.data).join(" ")}
+      </Typography>
     </div>
   );
 }
