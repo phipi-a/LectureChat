20a7111c0198e2fa587f54cd3dbc1a4c66568161
philipp <phpi@gmx.de>
2023-07-02 18:16:04 +0200

    add feature: create video room

--- webapp/src/app/host/room/[roomId]/page.tsx
+++ webapp/src/app/host/room/[roomId]/page.tsx
@@ -9,6 +9,9 @@ import { LiveAudioTranscriptionBox } from "@/common/components/host/roomPage/Liv
 import { CenteredLoading } from "@/common/components/general/CenteredLoading";
 import { PdfBulletpointContainerFallback } from "@/common/components/general/PdfBulletpointContainerFallback";
 import { LiveAudioTranscriptionBoxFallback } from "@/common/components/host/roomPage/LiveAudioTranscriptionBoxFallback";
+import { VideoTranscriptionBox } from "@/common/components/host/roomPage/VideoTranscriptionBox";
+import { VideoBulletPointContainer } from "@/common/components/general/VideoBulletpointContainer";
+import { Box } from "@mui/material";
 export default function Room({ params }: { params: { roomId: string } }) {
   const [page, setPage] = React.useState<number>(1);
   const [whisperUrl, setWhisperUrl] = React.useState<string>(
@@ -20,34 +23,57 @@ export default function Room({ params }: { params: { roomId: string } }) {
     supabase.from("room").select("*").eq("id", params.roomId).single()
   );
 
-  if (getRoom.isLoading) {
-    return <CenteredLoading />;
-  }
   const room = getRoom.data?.data;
 
   return (
-    <div>
-      <HostRoomHeader
-        title={room!.title}
-        roomId={room!.id}
-        password={room!.password}
-        whisperUrl={whisperUrl}
-        setWhisperUrl={setWhisperUrl}
-      />
-      <Suspense fallback={<LiveAudioTranscriptionBoxFallback />}>
-        <LiveAudioTranscriptionBox
-          roomId={params.roomId}
-          whisperUrl={whisperUrl}
-          page={page}
-        />
-      </Suspense>
-      <Suspense fallback={<PdfBulletpointContainerFallback />}>
-        <PdfBulletpointContainer
-          roomId={params.roomId}
-          page={page}
-          setPage={setPage}
-        />
-      </Suspense>
-    </div>
+    <>
+      {room?.is_video_room ? (
+        <Box>
+          <HostRoomHeader
+            title={room!.title}
+            roomId={room!.id}
+            password={room!.password}
+            whisperUrl={whisperUrl}
+            setWhisperUrl={setWhisperUrl}
+          />
+          <Suspense fallback={<></>}>
+            <VideoTranscriptionBox
+              roomId={params.roomId}
+              whisperUrl={whisperUrl}
+              page={page}
+            />
+          </Suspense>
+          <Box display={"inline-block"}>
+            <Suspense fallback={<></>}>
+              <VideoBulletPointContainer roomId={params.roomId} />
+            </Suspense>
+          </Box>
+        </Box>
+      ) : (
+        <div>
+          <HostRoomHeader
+            title={room!.title}
+            roomId={room!.id}
+            password={room!.password}
+            whisperUrl={whisperUrl}
+            setWhisperUrl={setWhisperUrl}
+          />
+          <Suspense fallback={<LiveAudioTranscriptionBoxFallback />}>
+            <LiveAudioTranscriptionBox
+              roomId={params.roomId}
+              whisperUrl={whisperUrl}
+              page={page}
+            />
+          </Suspense>
+          <Suspense fallback={<PdfBulletpointContainerFallback />}>
+            <PdfBulletpointContainer
+              roomId={params.roomId}
+              page={page}
+              setPage={setPage}
+            />
+          </Suspense>
+        </div>
+      )}
+    </>
   );
 }
